40ae84107e2a1ffdf4f64e27c053a798f326f4c4
Duy Tran <duytc@Duys-MacBook-Pro.local>
2023-12-02 12:02:55 +0700

    add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ComponentsComponent } from './components/components.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ComponentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
