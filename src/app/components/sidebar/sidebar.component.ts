65a5b5bff9f776e139ac48262f8e7e7b507ff96b
Duy Tran <duytc@Duys-MacBook-Pro.local>
2023-12-09 12:54:39 +0700

    done sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // Use const instead of let for constant variables
+    const arrows = document.querySelectorAll('.arrow');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarBtn = document.querySelector('.bx-menu');
 
+    // Use forEach for cleaner iteration over NodeList
+    arrows.forEach((arrow) => {
+      arrow.addEventListener('click', (event) => {
+        const arrowParent = (event as any).target.parentElement.parentElement;
+        arrowParent.classList.toggle('showMenu');
+      });
+    });
+
+    // Use optional chaining to simplify null/undefined checks
+    sidebarBtn?.addEventListener('click', () => {
+      sidebar?.classList.toggle('close');
+    });
+  }
 }
